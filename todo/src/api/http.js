f2239a3af1c8d2981a7dfe3fb1955ed417699ce2
Dmitriy <kalitvyansky.dmitry@gmail.com>
2022-06-09 16:42:45 +0300

    fix bugs

--- todo/src/api/http.js
+++ todo/src/api/http.js
@@ -1,9 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create ({
-    // baseURL: 'https://my-todo-list-server.herokuapp.com/'
-    baseURL: 'http://localhost:3002/'
-
+    baseURL: 'https://my-todo-list-server.herokuapp.com/'
 })
 
 export { http }
